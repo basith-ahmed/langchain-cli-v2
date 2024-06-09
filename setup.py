e1b010e8358cd31df65a16a921fc851cdbec9ead
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-06-10 05:25:56 +0530

    update: version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openwiz',
-    version='1.3.5',
+    version='1.3.6',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
